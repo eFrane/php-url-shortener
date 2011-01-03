8647a3fd47520815e2187d5339a47f4926a92741
Stefan Graupner <stefan.graupner@gmail.com>
2011-01-03 10:25:53 +0100

    inserted auth token.

--- config.php
+++ config.php
@@ -4,8 +4,18 @@ define('MYSQLI_HOST', 'localhost');
 define('MYSQLI_USER', 'user');
 define('MYSQLI_PASSWORD', 'password');
 define('MYSQLI_DATABASE', 'database');
-define('TWITTER_USERNAME', 'mathias');
-define('SHORT_URL', 'http://mths.be/'); // include the trailing slash!
-define('DEFAULT_URL', 'http://mathiasbynens.be'); // omit the trailing slash!
+define('TWITTER_USERNAME', '');
+define('SHORT_URL', ''); // include the trailing slash!
+define('DEFAULT_URL', ''); // omit the trailing slash!
+
+// auth token to make shortening more secure
+// esp. to avoid random ppl shortening URLs via this instance who shouldn't.
+if (!file_exists('auth.token')) {
+  if (!file_put_contents(sha1(uniqid().strftime(DATE_ATOM)))) {
+    die("Could neither access nor create auth.token, check config.");
+  }
+} else {
+  define('AUTH_TOKEN', trim(file_get_contents('auth.token')));
+}
 
 ?>
