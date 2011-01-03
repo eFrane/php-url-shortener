73273de7e3f780d56137905aac4e5b39149ac027
Stefan Graupner <stefan.graupner@gmail.com>
2011-01-03 16:32:22 +0100

    fixed url

--- mkfavelet.php
+++ mkfavelet.php
@@ -1,17 +1,24 @@
 <?php
-require('config.php');
+include('config.php');
 
 if (isset($argc) && isset($argv)) {
   // cli mode
+
+  $url = SHORT_URL.'favelet.php?token='.AUTH_TOKEN.'&url=\'+enc(d.location)+\'';
+
   $script =<<<SCRIPT
-  javascript:
-    window.open('%s', '_blank', 'width=480, height=100,
-      menubar=no,toolbar=no,status=no,location=no')
+  javascript:(
+    function() {
+      var%%20 d=document, wsh;
+      wsh = window.open('%s', '_blank', 'width=480, height=100,
+        menubar=no,toolbar=no,status=no,location=no');
+    })()
 SCRIPT;
 
   $script = preg_replace('/([ \n])*/', '', $script);
-  $script = sprintf($script, SHORT_URL.'favelet?auth='.AUTH_TOKEN);
+  $script = sprintf($script, $url);
+
   print $script."\n";
 } else {
-  die('CLI only.');
+  header('Location: /');
 }
