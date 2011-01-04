262b6daabfeff74d9b776b1fe8dcfa6b10d6ffd2
Stefan Graupner <stefan.graupner@gmail.com>
2011-01-04 12:58:33 +0100

    moved url shortening functions to lib.php

--- shorten.php
+++ shorten.php
@@ -1,5 +1,6 @@
 <?php
 include('config.php');
+include('lib.php');
 
 header('Content-Type: text/plain;charset=UTF-8');
 
@@ -14,33 +15,4 @@ if (in_array($url, array('', 'about:blank', 'undefined', 'http://localhost/'))) 
  die('Enter a URL.');
 }
 
-function nextLetter(&$str) {
- $str = ('z' === $str ? 'a' : ++$str);
-}
-
-function getNextShortURL($s) {
-  $i = base_convert($s, 36, 10);
-  return base_convert(++$i, 10, 36);
-}
-
-$db = new mysqli(MYSQLI_HOST, MYSQLI_USER, MYSQLI_PASSWORD, MYSQLI_DATABASE);
-$db->query('SET NAMES "utf8"');
-
-$url = $db->real_escape_string($url);
-
-$result = $db->query('SELECT `slug` FROM `redirect` WHERE `url` = "' . $url . '" LIMIT 1');
-if ($result && $result->num_rows > 0) { // If there’s already a short URL for this URL
- die(SHORT_URL . $result->fetch_object()->slug);
-} else {
- $result = $db->query('SELECT `slug`, `url` FROM `redirect` ORDER BY `date` DESC LIMIT 1');
- if ($result && $result->num_rows > 0) {
-  $slug = getNextShortURL($result->fetch_object()->slug);
-  if ($db->query('INSERT INTO `redirect` (`slug`, `url`, `date`, `hits`) VALUES ("' . $slug . '", "' . $url . '", NOW(), 0)')) {
-   header('HTTP/1.1 201 Created');
-   echo SHORT_URL . $slug;
-   $db->query('OPTIMIZE TABLE `redirect`');
-  }
- }
-}
-
-?>
+shorten($url);
