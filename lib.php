92fb344bf658acc02a7af120dd2e8e59f7fe54d6
Stefan Graupner <stefan.graupner@gmail.com>
2011-01-06 08:55:55 +0100

    fixed return value for existing short url

--- lib.php
+++ lib.php
@@ -27,20 +27,21 @@ function shorten($url, $echo = true) {
 
   $result = $db->query('SELECT `slug` FROM `redirect` WHERE `url` = "' . $url . '" LIMIT 1');
   if ($result && $result->num_rows > 0) { // If there’s already a short URL for this URL
-    die(SHORT_URL . $result->fetch_object()->slug);
+    $shorturl = SHORT_URL . $result->fetch_object()->slug);
   } else {
     $result = $db->query('SELECT `slug`, `url` FROM `redirect` ORDER BY `date` DESC LIMIT 1');
     if ($result && $result->num_rows > 0) {
       $slug = getNextShortURL($result->fetch_object()->slug);
       if ($db->query('INSERT INTO `redirect` (`slug`, `url`, `date`, `hits`) VALUES ("' . $slug . '", "' . $url . '", NOW(), 0)')) {
-        if ($echo) {
-          header('HTTP/1.1 201 Created');
-          echo SHORT_URL.$slug;
-          $db->query('OPTIMIZE TABLE `redirect`');
-        } else {
-          return SHORT_URL.$slug;
-        }
+        $db->query('OPTIMIZE TABLE `redirect`');
+        $shorturl = SHORT_URL.$slug;
       }
     }
+  }
+  if ($echo) {
+    header('HTTP/1.1 201 Created');
+    echo $shorturl;
+  } else {
+    return $shorturl;
   }
 }
